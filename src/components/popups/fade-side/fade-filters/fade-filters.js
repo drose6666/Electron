7d6bed56aabd3887d6927658c7dc2e1a5daa067b
Danila Rose <danila.rose.web@gmail.com>
2023-11-05 14:46:26 +0200

    добавлен фильтр товаров (каталог)

--- src/components/popups/fade-side/fade-filters/fade-filters.js
+++ src/components/popups/fade-side/fade-filters/fade-filters.js
@@ -6,3 +6,15 @@ new openFadeOnTheSide ({
    open: '.btn_favourites',
    overlay: '#overlay-filters',
 })
+
+const filterItem = document.querySelectorAll('.filter-collapse');
+const filterTitle = document.querySelectorAll('.filter-item__title')
+
+filterTitle.forEach(item => {
+   item.addEventListener('click', function () {
+      this.classList.toggle('hide')
+      let sib = this.nextElementSibling
+      sib.classList.toggle('hide')
+   })
+})
+
--- src/libs/js/module/accordion.js
+++ src/libs/js/module/accordion.js
@@ -1,6 +1,7 @@
-export default function Accordion ({ visible, collapse}) {
-   $(collapse).hide()
-
+export default function Accordion ({ visible, collapse, showAll = false}) {
+   
+   !showAll && $(collapse).hide()
+   
    $(visible).on('click', function () {
       $(visible).not($(this)).removeClass('active')
       $(collapse).not($(this).children(collapse)).slideUp()
