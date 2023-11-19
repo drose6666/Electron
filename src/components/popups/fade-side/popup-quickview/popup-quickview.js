6c922dfc741e77f4543405918aa3bbdcab954388
Danila Rose <danila.rose.web@gmail.com>
2023-11-19 22:54:16 +0200

    почти готово popup-quickview

--- src/components/popups/fade-side/popup-quickview/popup-quickview.js
+++ src/components/popups/fade-side/popup-quickview/popup-quickview.js
@@ -1,5 +1,41 @@
 import PopupHandler from '../../../../libs/js/module/fadeHiddenElements/PopupHandler.js'
 
+const options = {
+   slidesPerView: 1,
+   slidesPerGroup: 1,
+   spaceBetween: 5,
+   speed: 400,
+   pauseOnMouseEnter: true,
+   observer: true,
+   keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+   },
+   navigation: {
+      nextEl: '.quickview-product-next',
+      prevEl: '.quickview-product-prev'
+   },
+}
+
+let quickviewProductSwiper = null
+
+function initQuickviewProductSwiper () {
+   if (window.innerWidth <= 830) {
+      if (!quickviewProductSwiper) {
+         quickviewProductSwiper = new Swiper('#quickview-product-swiper', options)
+      }
+   } else {
+      if (quickviewProductSwiper) {
+         quickviewProductSwiper.destroy();
+         quickviewProductSwiper = null
+      }
+   }
+}
+
+window.addEventListener('load', initQuickviewProductSwiper)
+window.addEventListener('resize', initQuickviewProductSwiper)
+
+
 new PopupHandler ({
    selector: '#popup-quickview',
    btnClose: '.popup-quickview-close',
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,8 +1,8 @@
-import '../../components/ui/ui-preloader/ui-preloader.js';
 import './sections/new-products-section/new-products-section.js';
 import './sections/reviews-section/reviews-section.js';
 import '../../components/menu/mobile-menu/mobile-menu.js';
 import '../../components/ui//ui-product-counter/ui-product-counter.js';
 import '../../components/popups/popups.js';
+import '../../components/ui/ui-preloader/ui-preloader.js';
 
 // NOTE Временные импорты (для теста). Потом сделать каждый в нужном файле
