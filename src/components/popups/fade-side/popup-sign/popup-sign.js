a64c4363d913bb61a61d800b31ffc2e4d1f4a659
Danila Rose <danila.rose.web@gmail.com>
2023-11-21 00:17:24 +0200

    начало popup-sign

---
+++ src/components/popups/fade-side/popup-sign/popup-sign.js
@@ -0,0 +1,8 @@
+import PopupHandler from '../../../../libs/js/module/fadeHiddenElements/PopupHandler.js'
+
+new PopupHandler ({
+   selector: '#popup-sign',
+   btnClose: '.popup-sign-close',
+   btnOpen: '.btn_sign',
+   overlay: '#overlay-sign',
+})
--- src/components/popups/popups.js
+++ src/components/popups/popups.js
@@ -2,7 +2,8 @@ import './fade-side/popup-cart/popup-cart.js';
 import './fade-side/popup-compare/popup-compare.js';
 import './fade-side/popup-filters/popup-filters.js';
 import './fade-side/popup-review/popup-review.js';
-import './popup-send-review/popup-send-review.js'
+import './fade-side/popup-sign/popup-sign.js';
 import './fade-side/popup-search/popup-search.js';
 import './fade-side/popup-quickview/popup-quickview.js';
 import './popup-image-preview/popup-image-preview.js';
+import './popup-send-review/popup-send-review.js'
