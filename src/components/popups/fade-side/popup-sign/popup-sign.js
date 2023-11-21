ed6fc94dc01db0d672755f37928d83b16ead0bb0
Danila Rose <danila.rose.web@gmail.com>
2023-11-21 22:41:54 +0200

    форма входа + анимация переключения форм

--- src/components/popups/fade-side/popup-sign/popup-sign.js
+++ src/components/popups/fade-side/popup-sign/popup-sign.js
@@ -6,3 +6,20 @@ new PopupHandler ({
    btnOpen: '.btn_sign',
    overlay: '#overlay-sign',
 })
+
+const popupSign = document.getElementById('popup-sign');
+const dataFor = popupSign.querySelectorAll('[data-for]');
+const dataId = popupSign.querySelectorAll('[data-id]');
+
+dataFor.forEach(item => {
+   item.addEventListener('click', function () {
+      let currentDataForAttr = this.getAttribute('data-for')
+      let currentDataId = [...dataId].find(item => item.getAttribute('data-id') === currentDataForAttr)
+      
+      for (let dataIdItem of dataId) {
+         dataIdItem.classList.add('hidden')
+      }
+
+      currentDataId.classList.remove('hidden')
+   })
+})
