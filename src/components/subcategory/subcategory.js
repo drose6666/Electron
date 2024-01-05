722a2c740e4d04596356fc5fb20bfe3e28cecf17
Danila Rose <danila.rose.web@gmail.com>
2024-01-05 23:22:35 +0200

    изменена главная + чатсично страница категорий

--- src/components/subcategory/subcategory.js
+++ src/components/subcategory/subcategory.js
@@ -1,9 +1,9 @@
-window.addEventListener('load', function () {
-   const subcategoryList = document.querySelectorAll('.subcategory-list')
-   subcategoryList.forEach(item => {
-      let childsCount = item.children.length
+// window.addEventListener('load', function () {
+//    const subcategoryList = document.querySelectorAll('.subcategory-list')
+//    subcategoryList.forEach(item => {
+//       let childsCount = item.children.length
 
-      if (childsCount > 6) 
-         item.style.gridTemplate = `1fr 1fr / repeat(${Math.ceil(childsCount / 2)}, 1fr)`
-      })
-})
+//       if (childsCount > 4) 
+//          item.style.gridTemplate = `1fr / repeat(${Math.ceil(childsCount)}, 1fr)`
+//       })
+// })
