19db9a20ca68ececab07dd092c416e8e560a8fb9
Danila Rose <danila.rose.web@gmail.com>
2024-01-11 00:26:14 +0200

    page-order

---
+++ src/components/popups/popup-success-order/popup-success-order.js
@@ -0,0 +1,8 @@
+import PopupHandler from '../../../libs/js/module/fadeHiddenElements/PopupHandler.js';
+
+new PopupHandler ({
+   selector: '#popup-success-order',
+   btnClose: '.popup-success-order-close',
+   btnOpen: '.btn_success-order',
+   overlay: '#overlay-success-order',
+});
